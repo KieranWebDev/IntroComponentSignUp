cecc573ba12f1ffbc9dc8760d14ff9d0e1109d4f
Kieran <kieran.webdevelopment@gmail.com>
2022-11-23 18:54:06 +0000

    js fundamentals done

--- javascript.js
+++ javascript.js
@@ -3,7 +3,14 @@ const inputs = document.querySelectorAll('.input');
 const errorIcon = document.querySelectorAll('.err-icon');
 const span = document.querySelectorAll('.err-msg');
 
-const successMessage = document.querySelector('.sucessMsg');
+const successMessage = document.querySelector('.successMsg');
+
+// form values
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
 // const inputContainer = document.querySelectorAll('.input-container');
 
 // const firstName = document.querySelector('#first-name');
@@ -12,16 +19,20 @@ const successMessage = document.querySelector('.sucessMsg');
 
 // console.log(inputContainer);
 // console.log(span);
+// console.log(successMessage);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('value' + typeof inputs[0].value);
+
+  console.log('value ' + typeof inputs[0].value);
 
   let correctInputCount = 0;
   //   console.log(firstName.value);
-  console.log(span[0].textContent);
+  //   console.log(span[0].textContent);
+
   for (let i = 0; i < inputs.length; i++) {
     console.log(inputs[i].value);
+
     if (inputs[i].value === '') {
       errorIcon[i].classList.remove('hidden');
       inputs[i].classList.add('err-border');
@@ -38,6 +49,15 @@ form.addEventListener('submit', (e) => {
   }
   console.log(correctInputCount);
 
-  if 
-  console.log('success');
+  if (correctInputCount === 4) {
+    successMessage.textContent = `success! Enjoy your free trial ${firstName.value}`;
+    form.classList.add('hidden');
+    const submittedValues = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      password: password.value,
+    };
+    console.log(submittedValues);
+  }
 });
